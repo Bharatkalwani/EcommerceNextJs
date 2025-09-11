94037958c9734b220deafcf29a3334a51d22ad9e
Bharatkalwani <kalwanibharat40@gmail.com>
2025-09-11 16:38:07 +0530

    auth & main group added

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+export default function page(){
+
+    return (
+        <div>
+            <h1>login starts</h1>
+            <form action="submit">
+                <input type="text" />
+                   <input type="text" />
+                <button className="">Login</button>
+            </form>
+        </div>
+    )
+}
