67f344f3bc3f9428adcb1c4c759ac8cd693094fa
Bharatkalwani <kalwanibharat40@gmail.com>
2025-09-11 16:12:20 +0530

    initial commit & tailwind done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
- console.log("data")
+
   return (
   <main className="min-h-screen flex items-center justify-center bg-blue-100">
      <h1 className="text-4xl font-bold text-blue-700">
